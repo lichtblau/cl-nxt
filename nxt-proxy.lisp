2bc1827d730f44a5048e48e571f488622caab52d
David Lichteblau <david@lichteblau.com>
2014-04-21 13:31:32 +0300

    Mysterious local changes

--- nxt-proxy.lisp
+++ nxt-proxy.lisp
@@ -4,35 +4,48 @@
 
 (defun mux (tcp nxt)
   (loop
-   (when (let* ((length (read-little-endian-short tcp))
+     (unless
+	 (let* ((length (read-little-endian-short tcp))
 		(data (make-array length :element-type '(unsigned-byte 8))))
-	   (unless (plusp data)
+	   (unless (plusp length)
 	     (return))
 	   (read-sequence data tcp)
 	   (write-to-nxt nxt data)
 	   (logtest #x80 (elt data 0)))
-     ;; reply expected
-     (let ((data (read-from-nxt nxt)))
-       (write-to-nxt-us (length data) tcp)
-       (write-sequence data tcp)
-       (force-output tcp)))))
+       ;; reply expected
+       (let ((data (read-from-nxt nxt)))
+	 (write-to-nxt-us (length data) tcp)
+	 (write-sequence data tcp)
+	 (force-output tcp)))))
 
-(defun handle-connection (connection)
+(defun handle-connection (nxt connection)
+  (print (list nxt connection))
   (handler-case
-      (let ((stream (usocket:socket-stream connection))
-	    (nxt (nxt:open-connection '(:usb))))
-	(unwind-protect
-	     (mux stream nxt)
-	  (nxt:close-connection)))
+      (let ((stream (usocket:socket-stream connection)))
+	(mux stream nxt))
     (error (c)
       (format t "ignoring error: ~A~%" c))))
 
-(defun run-proxy (&optional (port 30000) (host usocket:*wildcard-host*))
-  (usocket:with-server-socket
-      (listener (usocket:socket-listen
-		 host port
-		 :reuse-address t
-		 :element-type '(unsigned-byte 8)))
-    (loop
-       (usocket:with-connected-socket (c (usocket:socket-accept listener))
-	 (handle-connection c)))))
+(defun beep (nxt freq dur)
+  (nxt-play-tone :nxt nxt :frequency freq :duration dur :onewayp t))
+
+(defun run-proxy
+  (&optional (port 30000) (host usocket:*wildcard-host*) (beep-on-startup t))
+  (let ((nxt (nxt:open-connection '(:usb))))
+    (unwind-protect
+	(usocket:with-server-socket
+	 (listener (usocket:socket-listen
+		    host port
+		    :reuse-address t
+		    :element-type '(unsigned-byte 8)))
+	 (when beep-on-startup
+	   (beep nxt 500 200))
+	 (loop
+	  (format t "accepting...~%")
+	  (with-simple-restart (abort "abort to nxt proxy")
+			       (usocket:with-connected-socket
+				(c (usocket:socket-accept listener))
+				(when beep-on-startup
+				  (beep nxt 800 100))
+				(handle-connection nxt c)))))
+      (nxt:close-connection))))
